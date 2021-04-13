62d3925134607766888bb96850967514def73f40
Sown1705 <sowntc2001@gmail.com>
2021-04-13 23:05:22 +0700

    update 13/04/2021

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
 
-//goi express
-var express=require('express');
-var expressHbs=require('express-handlebars');
-//tao app de cau hinh router
-var app = express();
-//chay len localhost
+//multer
+var multer =require('multer');
+var storage= multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'./public/uploads/')
+    },filename: function(req,file,cb){
+        const uniqueSuffix = Date.now()+'-'+Math.round(Math.random()*1E9)
+        cb(null,uniqueSuffix+'-'+file.originalname)
+    }
+})
 
-
-app.engine('handlebars',expressHbs({
-    layoutsDir:__dirname+'/views/layouts',
-    defaultLayout:'main'
+var upload = multer({
+    dest: './public/data/uploads/'
+    , storage: storage,
+    limits: {
+        fileSize: 1 * 1024 * 1024, // gioi han file size <= 1MB
+    }
+}).single('avatar')
+// Configure template Engine and Main Template File
+app.engine('handlebars', exphbs({
+    layoutsDir:__dirname + '/views/layouts',
+    defaultLayout: 'main',
 }));
-app.use(express.static('assets'));
+// Setting template Engine
 app.set('view engine', 'handlebars');
-app.get('/', function(req, res){
-        res.render('index');
+
+app.use(express.static( 'public'));
+// routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.get('/swiper', (req, res) => {
+    res.render('swiper');
+});
+app.get('/profile', (req, res) => {
+    res.render('profile');
+});
+app.get('/upload', (req, res) => {
+    res.render('upload');
 });
 
-app.get('/chat', function(req, res){
-     res.render('chat');
+//upload
+app.post('/uploadImage', function (req, res) {
+    // req.file is the name of your file in the form above, here 'uploaded_file'
+    // req.body will hold the text fields, if there were any
+    console.log(req.file, req.body)
+     upload(req, res, function (error) {
+        if (error instanceof multer.MulterError) {
+            return res.send("File size Maximum is 1MB.Please try again!!!")
+        } else {
+            return res.send(error);
+        }
+    });
 });
-
-app.get('/login', function(req, res){
-     res.render('login');
+app.listen(3000, () => {
+    console.log('The web server has started on port 3000');
 });
-app.listen(process.env.PORT||3001);
-
